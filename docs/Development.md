84c930506d426ea6c321aae197d5b435aa69aae0
Youyuan Wu <youyuanwu@outlook.com>
2024-03-16 12:36:15 -0700

    enable github actions

---
+++ docs/Development.md
@@ -0,0 +1,5 @@
+# MISC
+To run tests locally on linux, run:
+```sh
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/microsoft/servicefabric/bin/Fabric/Fabric.Code:"
+```
---
+++ scripts/kvstore_ctl.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+action=""
+verbose=0
+
+while getopts "h?va:" opt; do
+  case "$opt" in
+    h|\?)
+      show_help
+      exit 0
+      ;;
+    v)  verbose=1
+      ;;
+    a)  action=$OPTARG
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+echo "verbose=$verbose, action='$action', Leftovers: $@"
+
+if [ "$action" == "" ]; then
+  echo "action is empty"
+  exit 1
+fi
+
+if [ "$action" == "add" ]; then
+  echo "add"
+  sfctl cluster select
+  sfctl application upload --path build/kvstore_root
+  sfctl application provision --application-type-build-path kvstore_root
+  sfctl application create --app-name fabric:/KvStore --app-type KvStore --app-version 0.0.1
+elif [ "$action" == "remove" ]; then
+  echo "remove"
+  sfctl cluster select
+  sfctl application delete --application-id KvStore
+  sfctl application unprovision --application-type-name KvStore --application-type-version  0.0.1
+else
+  echo "unknown command"
+  exit 1
+fi
