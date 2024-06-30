aae08f4397a7df3a5915fb8b22e6b5edd324fcfa
Youyuan Wu <youyuanwu@outlook.com>
2024-06-30 14:20:35 -0700

    copy kvstore base implementation

---
+++ crates/samples/kvstore/src/kvstore.rs
@@ -0,0 +1,240 @@
+use std::{cell::Cell, sync::Mutex};
+
+use mssf_com::{
+    FabricRuntime::{
+        IFabricKeyValueStoreReplica2, IFabricStatefulServiceReplica, IFabricStoreEventHandler,
+    },
+    FabricTypes::FABRIC_REPLICATOR_ADDRESS,
+};
+use mssf_core::{
+    runtime::{
+        executor::{DefaultExecutor, Executor},
+        stateful::{
+            PrimaryReplicator, StatefulServiceFactory, StatefulServicePartition,
+            StatefulServiceReplica,
+        },
+        stateful_proxy::StatefulServiceReplicaProxy,
+        stateful_types::{OpenMode, Role},
+        store::{create_com_key_value_store_replica, DummyStoreEventHandler},
+        store_proxy::KVStoreProxy,
+        store_types::ReplicatorSettings,
+    },
+    Error, GUID, HSTRING,
+};
+use tokio::{
+    select,
+    sync::oneshot::{self, Sender},
+};
+use tracing::info;
+use windows_core::Interface;
+
+pub struct Factory {
+    replication_port: u32,
+    rt: DefaultExecutor,
+}
+
+impl Factory {
+    pub fn create(replication_port: u32, rt: DefaultExecutor) -> Factory {
+        Factory {
+            replication_port,
+            rt,
+        }
+    }
+}
+
+fn get_addr(port: u32, hostname: HSTRING) -> String {
+    let mut addr = String::new();
+    addr.push_str(&hostname.to_string());
+    addr.push(':');
+    addr.push_str(&port.to_string());
+    addr
+}
+
+impl StatefulServiceFactory for Factory {
+    fn create_replica(
+        &self,
+        servicetypename: &HSTRING,
+        servicename: &HSTRING,
+        initializationdata: &[u8],
+        partitionid: &GUID,
+        replicaid: i64,
+    ) -> Result<impl StatefulServiceReplica, Error> {
+        info!(
+            "Factory::create_replica type {}, service {}, init data size {}",
+            servicetypename,
+            servicename,
+            initializationdata.len()
+        );
+        let settings = ReplicatorSettings {
+            Flags: FABRIC_REPLICATOR_ADDRESS.0 as u32,
+            ReplicatorAddress: HSTRING::from(get_addr(self.replication_port, "localhost".into())),
+            ..Default::default()
+        };
+
+        info!(
+            "Factory::create_replica using address {}",
+            settings.ReplicatorAddress
+        );
+
+        let dbname = get_service_name(servicename.to_string()).expect("servicename is irregular");
+
+        let handler: IFabricStoreEventHandler = DummyStoreEventHandler {}.into();
+        let kv = create_com_key_value_store_replica(
+            &HSTRING::from(dbname.clone()),
+            *partitionid,
+            replicaid,
+            &settings,
+            mssf_core::runtime::store_types::LocalStoreKind::Ese,
+            None,
+            &handler,
+        )?;
+        let kv_replica: IFabricStatefulServiceReplica = kv.clone().cast().unwrap();
+        let proxy = StatefulServiceReplicaProxy::new(kv_replica);
+
+        let svc = Service::new(kv, self.rt.clone(), dbname);
+
+        let replica = Replica::new(proxy, svc);
+        Ok(replica)
+    }
+}
+
+pub struct Replica {
+    kv: StatefulServiceReplicaProxy,
+    svc: Service,
+}
+
+impl Replica {
+    pub fn new(kv: StatefulServiceReplicaProxy, svc: Service) -> Replica {
+        Replica { kv, svc }
+    }
+}
+
+// The serving of the database.
+pub struct Service {
+    kvproxy: KVStoreProxy,
+    rt: DefaultExecutor,
+    tx: Mutex<Cell<Option<Sender<()>>>>,
+    dbname: String,
+}
+
+impl Service {
+    pub fn new(com: IFabricKeyValueStoreReplica2, rt: DefaultExecutor, dbname: String) -> Service {
+        Service {
+            kvproxy: KVStoreProxy::new(com),
+            rt,
+            tx: Mutex::new(Cell::new(None)),
+            dbname,
+        }
+    }
+
+    pub fn start_loop(&self) {
+        let (tx, mut rx) = oneshot::channel::<()>();
+        let kv = self.kvproxy.clone();
+        self.stop();
+        self.tx.lock().unwrap().set(Some(tx));
+        let dbname = self.dbname.clone();
+        self.rt.spawn(async move {
+            let mut counter = 0;
+            loop {
+                info!("Service::run_single for db {}: {}", dbname, counter);
+                let res = Self::run_single(&kv).await;
+                match res {
+                    Ok(_) => info!("run_single success"),
+                    Err(e) => info!("run_single error : {}", e),
+                }
+                counter += 1;
+                // sleep or stop
+                select! {
+                    _ = tokio::time::sleep(std::time::Duration::from_secs(10)) => {
+                        continue;
+                    }
+                    _ = &mut rx =>{
+                        info!("Service::loop stopped from rx");
+                        break;
+                    }
+                }
+            }
+        });
+    }
+
+    pub fn stop(&self) {
+        let mut op = self.tx.lock().unwrap().take();
+        if op.is_some() {
+            op.take().unwrap().send(()).unwrap()
+        }
+    }
+
+    async fn run_single(kv: &KVStoreProxy) -> mssf_core::Result<()> {
+        // add kv
+        let seq;
+        {
+            let tx = kv.create_transaction()?;
+            let key = HSTRING::from("mykey");
+            let value = String::from("myvalue");
+            kv.add(&tx, key.as_wide(), value.as_bytes())?;
+            seq = tx.commit(1000).await?;
+        }
+
+        // remove kv
+        {
+            let tx = kv.create_transaction()?;
+            let key = HSTRING::from("mykey");
+            kv.remove(&tx, key.as_wide(), seq)?;
+            let _ = tx.commit(1000).await?;
+        }
+        Ok(())
+    }
+}
+
+impl StatefulServiceReplica for Replica {
+    async fn open(
+        &self,
+        openmode: OpenMode,
+        partition: &StatefulServicePartition,
+    ) -> mssf_core::Result<impl PrimaryReplicator> {
+        // should be primary replicator
+        info!("Replica::open {:?}", openmode);
+        self.kv.open(openmode, partition).await
+    }
+    async fn change_role(&self, newrole: Role) -> mssf_core::Result<HSTRING> {
+        info!("Replica::change_role {:?}", newrole);
+        let addr = self.kv.change_role(newrole.clone()).await?;
+        if newrole == Role::Primary {
+            self.svc.start_loop();
+        }
+        Ok(addr)
+    }
+    async fn close(&self) -> mssf_core::Result<()> {
+        info!("Replica::close");
+        self.svc.stop();
+        self.kv.close().await
+    }
+    fn abort(&self) {
+        info!("Replica::abort");
+        self.svc.stop();
+        self.kv.abort();
+    }
+}
+
+// returns the service name
+// fabric:/KvStore/KvStoreService -> KvStoreService
+fn get_service_name(url: String) -> Option<String> {
+    let parts = url.split('/').collect::<Vec<_>>();
+    if parts.len() != 3 {
+        None
+    } else {
+        Some(parts[2].to_owned())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::get_service_name;
+
+    #[test]
+    fn url_parse() {
+        let url = "fabric:/KvStore/KvStoreService";
+        let name = get_service_name(url.to_owned()).unwrap();
+        assert_eq!(name, "KvStoreService");
+    }
+}
---
+++ crates/samples/kvstore/src/main.rs
@@ -0,0 +1,47 @@
+use mssf_core::HSTRING;
+use mssf_core::{
+    debug::wait_for_debugger,
+    runtime::{
+        executor::{DefaultExecutor, Executor},
+        ActivationContext,
+    },
+};
+use tracing::info;
+
+use crate::kvstore::Factory;
+
+mod kvstore;
+
+fn has_debug_arg() -> bool {
+    let args: Vec<String> = std::env::args().collect();
+    for arg in args {
+        if arg == "-WaitForDebugger" {
+            return true;
+        }
+    }
+    false
+}
+
+fn main() -> mssf_core::Result<()> {
+    tracing_subscriber::fmt().init();
+    info!("main start");
+    if has_debug_arg() {
+        wait_for_debugger();
+    }
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let e = DefaultExecutor::new(rt.handle().clone());
+    let runtime = mssf_core::runtime::Runtime::create(e.clone()).unwrap();
+    let actctx = ActivationContext::create().unwrap();
+    let endpoint = actctx
+        .get_endpoint_resource(&HSTRING::from("KvReplicatorEndpoint"))
+        .unwrap();
+
+    let factory = Factory::create(endpoint.Port, e.clone());
+    runtime
+        .register_stateful_service_factory(&HSTRING::from("KvStoreService"), factory)
+        .unwrap();
+
+    e.run_until_ctrl_c();
+    Ok(())
+}
