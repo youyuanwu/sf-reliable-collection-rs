76dd4402b1ab6ec33346fed824559a70c0fa5b49
Youyuan Wu <youyuanwu@outlook.com>
2024-06-29 22:55:33 -0700

    change license to Microsoft MIT

--- crates/libs/c/src/lib.rs
+++ crates/libs/c/src/lib.rs
@@ -1,5 +1,5 @@
 // ------------------------------------------------------------
-// Copyright 2024 Youyuan Wu
+// Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License (MIT). See License in the repo root for
 // license information.
 // ------------------------------------------------------------
--- crates/samples/kvstore/kvcli/main.rs
+++ crates/samples/kvstore/kvcli/main.rs
@@ -1,3 +1,9 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License in the repo root for
+// license information.
+// ------------------------------------------------------------
+
 use std::time::Duration;
 
 use clap::Parser;
