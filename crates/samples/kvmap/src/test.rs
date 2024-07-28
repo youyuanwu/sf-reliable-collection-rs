f65eb50acb9d893288df081d52a7031ed319454d
Youyuan Wu <youyuanwu@outlook.com>
2024-07-28 00:23:45 -0700

    refactor test for kvmap

--- crates/samples/kvmap/src/test.rs
+++ crates/samples/kvmap/src/test.rs
@@ -2,50 +2,109 @@ use std::time::Duration;
 
 use mssf_core::{
     client::{
-        svc_mgmt_client::{PartitionKeyType, ServiceEndpointRole},
+        query_client::QueryClient,
+        svc_mgmt_client::{PartitionKeyType, ServiceEndpointRole, ServiceManagementClient},
         FabricClient,
     },
-    HSTRING,
+    types::{
+        ServicePartition, ServicePartitionInformation, ServicePartitionQueryDescription,
+        ServicePartitionStatus,
+    },
+    GUID, HSTRING,
 };
+use tokio::sync::Semaphore;
 
 use crate::rpc::{DataSnPayload, EmptyPayload};
+use lazy_static::lazy_static;
+
+// limit 1 test at a time.
+static PERMIT: Semaphore = Semaphore::const_new(1);
+static TIMEOUT: Duration = Duration::from_secs(2);
+lazy_static! {
+    static ref KV_MAP_SVC_URI: HSTRING = HSTRING::from("fabric:/KvMap/KvMapService");
+    static ref FABRIC_CLIENT: FabricClient = FabricClient::new();
+}
+// helper for managing app
+pub struct KvMapMgmt {
+    svc: ServiceManagementClient,
+    query: QueryClient,
+}
+
+impl KvMapMgmt {
+    pub fn new(c: &FabricClient) -> Self {
+        Self {
+            svc: c.get_service_manager(),
+            query: c.get_query_manager(),
+        }
+    }
+
+    // first is primary
+    pub async fn get_addrs(&self) -> (String, String) {
+        let resolution = self
+            .svc
+            .resolve_service_partition(
+                &KV_MAP_SVC_URI,
+                &PartitionKeyType::None,
+                None,
+                Duration::from_secs(1),
+            )
+            .await
+            .unwrap();
+        // find endpoints
+        let endpoints = resolution.get_endpoint_list();
+
+        // there is only 2 replicas
+
+        let primary_addr = endpoints
+            .iter()
+            .find(|e| e.role == ServiceEndpointRole::StatefulPrimary)
+            .expect("no primary found")
+            .address
+            .to_string();
+        let secondary_addr = endpoints
+            .iter()
+            .find(|e| e.role == ServiceEndpointRole::StatefulSecondary)
+            .expect("no secondary found")
+            .address
+            .to_string();
+        (primary_addr, secondary_addr)
+    }
+
+    pub async fn get_partition(&self) -> (GUID, ServicePartitionStatus) {
+        let desc = ServicePartitionQueryDescription {
+            service_name: KV_MAP_SVC_URI.clone(),
+            partition_id_filter: None,
+        };
+        let list = self.query.get_partition_list(&desc, TIMEOUT).await.unwrap();
+        // there is only one partition
+        let p = list.iter().next().unwrap();
+        let stateful = match p {
+            ServicePartition::Stateful(s) => s,
+            _ => panic!("not stateless"),
+        };
+        let info = stateful.partition_information;
+        let single = match info {
+            ServicePartitionInformation::Singleton(s) => s,
+            _ => panic!("not singleton"),
+        };
+        (single.id, stateful.partition_status)
+    }
+}
 
 #[tokio::test]
 async fn read_write_test() {
-    // resolve port on local onebox
-    let fc = FabricClient::new();
-    let svcc = fc.get_service_manager();
-    let resolution = svcc
-        .resolve_service_partition(
-            &HSTRING::from("fabric:/KvMap/KvMapService"),
-            &PartitionKeyType::None,
-            None,
-            Duration::from_secs(1),
-        )
-        .await
-        .unwrap();
-    // find endpoints
-    let endpoints = resolution.get_endpoint_list();
+    let _ = PERMIT.acquire().await.unwrap();
 
-    let primary_addr = endpoints
-        .iter()
-        .find(|e| e.role == ServiceEndpointRole::StatefulPrimary)
-        .expect("no primary found")
-        .address
-        .to_string();
+    // resolve port on local onebox
+    let c = KvMapMgmt::new(&FABRIC_CLIENT);
+    let (primary_addr, secondary_addr) = c.get_addrs().await;
+
     println!("primary_addr: {}", primary_addr);
     // connect primary via grpc
     let mut client = crate::rpc::kvmap_service_client::KvmapServiceClient::connect(primary_addr)
         .await
         .unwrap();
-
     // connect secondary
-    let secondary_addr = endpoints
-        .iter()
-        .find(|e| e.role == ServiceEndpointRole::StatefulSecondary)
-        .expect("no secondary found")
-        .address
-        .to_string();
     println!("secondary_addr: {}", secondary_addr);
     let mut sec_client =
         crate::rpc::kvmap_service_client::KvmapServiceClient::connect(secondary_addr)
@@ -87,4 +146,13 @@ async fn read_write_test() {
             println!("RESPONSE={:?}", response2);
         }
     }
+}
+
+// TODO: perform failover.
+#[tokio::test]
+async fn failover_test() {
+    let _ = PERMIT.acquire().await.unwrap();
+    let c = KvMapMgmt::new(&FABRIC_CLIENT);
+    let (_, status) = c.get_partition().await;
+    assert_eq!(status, ServicePartitionStatus::Ready)
 }
